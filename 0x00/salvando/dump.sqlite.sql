a45e6296704a5eb1a476d6cf1ef020506ddbecc3
Cesar A. Pires <cesarp@ciandt.com>
2022-06-21 16:17:47 -0300

    Projeto dia 33 Tasks

---
+++ 0x00/salvando/dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE CLIENTE (
+   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+   nome VARCHAR(80) NOT NULL,
+   email VARCHAR(40) NOT NULL,
+   telefone int
+   );
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
